f6190639f90ceda0b53a0675cbe244711e45e8e4
swase <fg2g15@soton.ac.uk>
2021-09-16 12:29:48 +0100

    Added starlink service, model and callmanager code for starlink request

--- APIMiniProject/APIMiniProject/HTTPManager/CallManager.cs
+++ APIMiniProject/APIMiniProject/HTTPManager/CallManager.cs
@@ -20,6 +20,18 @@ namespace APIMiniProject.HTTPManager
             _client = new RestClient(uri);
         }
 
+        public async Task<string> MakeStarLinkRequest(string sateliteID)
+        {
+            //set up the request
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            //define the request resource path
+            request.Resource = $"/starlink/{sateliteID}";
 
+            //Make request, (doesn't instantiate the interface. Just uses as type
+            IRestResponse response = await _client.ExecuteAsync(request);
+            StatusCode = (int)response.StatusCode;
+            return response.Content;
+        }
     }
 }
---
+++ APIMiniProject/APIMiniProject/Services/StarlinkSingleSateliteServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using APIMiniProject.Data;
+using System.Threading.Tasks;
+using APIMiniProject.HTTPManager;
+using Newtonsoft.Json.Linq;
+
+namespace APIMiniProject.Services
+{
+    class StarlinkSingleSateliteServices
+    {
+        #region Properties
+        public CallManager CallManager { get; set; }
+        public JObject Json_Response { get; set; }
+        public DTO<StarlinkSingleResponse> SingleStarlinkDTO { get; set; }
+        public string SateliteSelected { get; set; }
+        public string StarlinkResponse { get; set; }
+        #endregion
+
+        public StarlinkSingleSateliteServices()
+        {
+            CallManager = new CallManager();
+            SingleStarlinkDTO = new DTO<StarlinkSingleResponse>();
+
+        }
+
+        public async Task MakeRequest(string sateliteID)
+        {
+            StarlinkResponse = await CallManager.MakeStarLinkRequest(sateliteID);
+            SateliteSelected = sateliteID;
+            Json_Response = JObject.Parse(StarlinkResponse);
+            SingleStarlinkDTO.Deserialize(StarlinkResponse);
+        }
+
+    }
+}
