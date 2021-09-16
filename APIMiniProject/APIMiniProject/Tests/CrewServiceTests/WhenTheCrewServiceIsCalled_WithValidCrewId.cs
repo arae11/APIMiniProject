e51e6ac23ddb999042099e84b8bf4e7b7bd10eb0
arthur tshaka <arthurltshaka@gmail.com>
2021-09-16 15:10:02 +0100

    first crew test

---
+++ APIMiniProject/APIMiniProject/Tests/CrewServiceTests/WhenTheCrewServiceIsCalled_WithValidCrewId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIMiniProject.Data;
+using APIMiniProject.HTTPManager;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using APIMiniProject.Services;
+
+namespace APIMiniProject.Tests.CrewServiceTests
+{
+	class WhenTheCrewServiceIsCalled_WithValidCrewId
+	{
+		private CrewService _crewService;
+		[OneTimeSetUp]
+
+		public async Task OneTimeSetupAsync()
+		{
+			_crewService = new CrewService();
+			await _crewService.MakeRequestAsync("5ebf1b7323a9a60006e03a7b");
+		}
+
+		[Test]
+
+		public void StatusIs200()
+		{
+			Assert.That(_crewService.CallManager.StatusCode, Is.EqualTo(200));
+		}
+
+		/*
+		[Test]
+		public void StatusIs200_alt()
+		{
+			Assert.That(_crewService.CrewResponse)
+		}
+		*/
+	}
+}
