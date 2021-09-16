33a5c5e1ae0f1fd42f3cc854a203e72c51f231fb
Gareth <GMitchell@Spartaglobal.com>
2021-09-16 12:10:29 +0100

    Added Capsule Service

---
+++ APIMiniProject/APIMiniProject/Services/CapsuleService.cs
@@ -0,0 +1,37 @@
+﻿using APIMiniProject.Data;
+using APIMiniProject.HTTPManager;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject.Services
+{
+    public class CapsuleService
+    {
+        public CallManager CallManager { get; set; }
+        public JObject JsonResponse { get; set; }
+        public DTO<CapsuleResponse> CapsuleResponseDTO { get; set; }
+        public string CapsuleSelected { get; set; }
+        public string CapsuleResponse { get; set; }
+
+        public CapsuleService()
+        {
+            CallManager = new CallManager();
+            CapsuleResponseDTO = new DTO<CapsuleResponse>();
+        }
+
+        public async Task MakeRequestAsync(string capsuleID)
+        {
+            CapsuleSelected = capsuleID;
+            CapsuleResponse = await CallManager.MakeCapsuleRequestAsync(capsuleID);
+
+            JsonResponse = JObject.Parse(CapsuleResponse);
+
+            CapsuleResponseDTO.Deserialize(CapsuleResponse);
+        }
+
+    }
+}
